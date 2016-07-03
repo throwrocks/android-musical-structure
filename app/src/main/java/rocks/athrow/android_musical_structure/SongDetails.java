c3775c6794ad32956cc0e08187ee1b7eda0d3f54
Jose E Lopez <p0p.thr0w@gmail.com>
2016-07-03 14:17:21 -0500

    Completed the SongDetails activity.

--- app/src/main/java/rocks/athrow/android_musical_structure/SongDetails.java
+++ app/src/main/java/rocks/athrow/android_musical_structure/SongDetails.java
@@ -1,7 +1,9 @@
 package rocks.athrow.android_musical_structure;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by josel on 7/3/2016.
@@ -12,5 +14,11 @@ public class SongDetails extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.song_details);
+    }
+
+    public void openRelatedSong(View view){
+        Intent songDetails = new Intent(this, SongDetails.class);
+        startActivity(songDetails);
+        finish();
     }
 }
--- app/src/main/java/rocks/athrow/android_musical_structure/SongsList.java
+++ app/src/main/java/rocks/athrow/android_musical_structure/SongsList.java
@@ -1,10 +1,7 @@
 package rocks.athrow.android_musical_structure;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
